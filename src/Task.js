eff89d5dd1eade16eccfefa616a3b4a14767623f
Dinda Nurafni <dindanurafni205@gmail.com>
2023-06-16 21:39:52 +0700

    deploy

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Dinda Nurafni" // TODO: replace this
+const studentId = "FE3573074" // TODO: replace this
+
+export { url, studentName, studentId }
